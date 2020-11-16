321561b932bcbea1dba62c564655e24637c0d0a6
grits <grits.0123@gmail.com>
2020-11-16 20:51:59 +0300

    @EnableJpaRepositories.

--- src/main/java/com/example/SaveApplication.java
+++ src/main/java/com/example/SaveApplication.java
@@ -2,13 +2,17 @@ package com.example;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @SpringBootApplication
+@EnableJpaRepositories
 public class SaveApplication {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(SaveApplication.class, args);
