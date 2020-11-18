e0f37d39c601d6173ec828ca8c7ed375143c7f45
grits <grits.0123@gmail.com>
2020-11-18 16:44:36 +0300

    Added NewsService.

--- src/main/java/com/example/controller/NewsController.java
+++ src/main/java/com/example/controller/NewsController.java
@@ -4,6 +4,7 @@ package com.example.controller;
 import com.example.entity.News;
 import com.example.newsApi.NewsAPI;
 import com.example.response.RestResponse;
+import com.example.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,9 @@ public class NewsController {
     @Autowired
     NewsAPI newsAPI;
 
+    @Autowired
+    NewsService newsService;
+
 
     @RequestMapping(value = "/get-news", method = RequestMethod.GET)
     public RestResponse<List<News>> getNewsByCategory(@RequestParam("category") String category) {
@@ -28,5 +32,16 @@ public class NewsController {
             return new RestResponse<>(null, "NEWS PROBLEM", 12);
 
         return new RestResponse<List<News>>(news, "ALL RIGHT", 0);
+    }
+
+
+    @RequestMapping(value = "/is-liked", method = RequestMethod.GET)
+    public RestResponse<Boolean> getIsLiked(@RequestParam("id") Integer id) {
+        Boolean isLiked = newsService.isLiked(id);
+
+        if(!isLiked)
+            return new RestResponse<Boolean>(false, "NEWS IS NOT LIKED", 0);
+
+        return new RestResponse<Boolean>(true, "NEWS IS LIKED", 0);
     }
 }
