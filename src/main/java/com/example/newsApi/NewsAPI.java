0f4991f34dbe2cc718b43e22ed2db889f7d8f8d3
grits <grits.0123@gmail.com>
2020-11-20 10:51:57 +0300

    News layer for RESTful api.

--- src/main/java/com/example/newsApi/NewsAPI.java
+++ src/main/java/com/example/newsApi/NewsAPI.java
@@ -22,26 +22,23 @@ public class NewsAPI {
     private static final String API_KEY = "&apiKey=a8ab51cc7ddb41fc9e63126e99b106bd";
 
 
-    public String formatContent(String content, int howMuchSentence) {
-        String[] contentParts = content.split(".", howMuchSentence);
-
+    public String formatContent(String content, int howMuchWords) {
+        String[] contentParts = content.split(" ", howMuchWords);
         StringBuffer result = new StringBuffer();
 
-        for (int i = 0; i < contentParts.length; i++)
+        for (int i = 0; i < contentParts.length; i++) {
             result.append(contentParts[i]);
-
-        for (String string : contentParts) {
-            System.out.println(string);
         }
 
-        System.out.println(result.toString());
+        System.out.println(contentParts.length);
+        System.out.println(result);
 
         return result.toString();
     }
 
 
     public List<News> getNewsByCategory(String category)  {
-        News news = new News();
+        News news;
         List<News> newsList = new ArrayList<News>();
 
         URL url = null;
@@ -63,9 +60,6 @@ public class NewsAPI {
 
         } catch (IOException e) { e.printStackTrace(); }
 
-
-        System.out.println(new JSONObject(content.toString()));
-
         JSONObject jsonObject = new JSONObject(content.toString());
         JSONObject article = new JSONObject();
 
@@ -74,14 +68,16 @@ public class NewsAPI {
 
         for (int i = 0; i < articles.length(); i++) {
             article = articles.getJSONObject(i);
+            news = new News();
 
             news.setPublishedAt(article.optString("publishedAt"));
             news.setAuthor(article.optString("author").toString());
             news.setUrlToImage(article.optString("urlToImage").toString());
-            news.setDescription(formatContent(article.optString("description").toString(), 2));
-            news.setSourceName(article.optString("sourceName"));
+            news.setDescription(formatContent(article.optString("description"), 2));
+            news.setSourceName(article.getJSONObject("source").optString("sourceName"));
             news.setTitle(article.optString("title").toString());
             news.setUrl(article.optString("url").toString());
+            news.setCategory(category);
 
             newsList.add(news);
         }
